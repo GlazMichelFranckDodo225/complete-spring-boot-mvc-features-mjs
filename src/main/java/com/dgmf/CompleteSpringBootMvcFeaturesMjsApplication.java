64428d1ea44d802920b15c4296859dcf1dfc5b72
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-11-27 04:40:06 +0100

    Project Initialization

---
+++ src/main/java/com/dgmf/CompleteSpringBootMvcFeaturesMjsApplication.java
@@ -0,0 +1,13 @@
+package com.dgmf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompleteSpringBootMvcFeaturesMjsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompleteSpringBootMvcFeaturesMjsApplication.class, args);
+	}
+
+}
